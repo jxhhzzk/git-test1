c7a547015a935833d62f897176367d99064ee801
jxhh <1719985134@qq.com>
2022-04-29 22:53:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * ClassName:GitTest
+ * Package:com.atguigu.git
+ * Description:
+ *
+ * @Date: 2022/4/29 22:25
+ * @Author: zhuzhik
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
